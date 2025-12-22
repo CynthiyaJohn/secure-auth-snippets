9bd7020604959ddb41e6b3f4a32087e49dfc94f9
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2025-12-22 23:04:21 +0530

    Create SecureSessionToken.java

---
+++ java/SecureSessionToken.java
@@ -0,0 +1,14 @@
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class SecureSessionToken {
+
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[32];
+        random.nextBytes(bytes);
+
+        String token = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+        System.out.println(token);
+    }
+}
