52ffff4de277e49aca144845cf925a21e7d686e0
Sharon Cynthiya J <145119901+CynthiyaJohn@users.noreply.github.com>
2025-12-25 23:39:26 +0530

    Create ReplayAttackSimulation.java

---
+++ java/ReplayAttackSimulation.java
@@ -0,0 +1,6 @@
+public class ReplayAttackSimulation {
+    public static void main(String[] args) {
+        String token = "static-token";
+        System.out.println("Reused token: " + token);
+    }
+}
